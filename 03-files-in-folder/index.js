e5092b77da91070f1ecbdfc573cfeaf626c36a1d
MAX <debaev@list.ru>
2021-11-07 14:30:34 +0500

    fix: path

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 const fsPromise = require('fs').promises;
 const path = require('path');
+const fileSrc = path.join('03-files-in-folder','secret-folder')
 
 async function getFolderFiles(folderName) {
   const files = await fsPromise.readdir(folderName, {withFileTypes: true});
   
   for (const file of files) {
-    fs.stat(`./03-files-in-folder/secret-folder/${file.name}`, (err, stats) => {
+    fs.stat(path.join(fileSrc,`${file.name}`), (err, stats) => {
       err ? console.log(err) : '';
       stats.isFile() ? process.stdout.write(`${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - ${stats.size / 1000}kb\n`) : '';
     })
   }
 }
 
-getFolderFiles(`${__dirname}/secret-folder`);
+getFolderFiles(path.join(__dirname,'secret-folder'));
