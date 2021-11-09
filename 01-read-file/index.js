8c192faa2b219b3ffd65b2316decb5f6e3342c92
MAX <debaev@list.ru>
2021-11-09 05:46:01 +0500

    msg: add msg

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,3 +3,6 @@ const path = require('path');
 
 let readStream = fs.createReadStream(path.join(__dirname,'text.txt'),'utf8');
 readStream.on('data', result => process.stdout.write(result.trim()));
+
+
+console.log('Все пункты сделаны если вдруг найдете ошибку напишите мне пожалуйста я до дедлайна постараюсь исправить заранее спасибо за понимание!');
